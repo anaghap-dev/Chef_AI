84274cb08eaca43ffd2bdb216e47db2551326402
emianna2005 <emiliaannaantony@gmail.com>
2026-03-12 11:36:52 +0530

    Fix merge conflict in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-=======
-
->>>>>>> 4c04940fbf6fc09b1049bccb8b98e6a589491d12
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-reportWebVitals();
-=======
-
->>>>>>> 4c04940fbf6fc09b1049bccb8b98e6a589491d12
+// Optional performance logging
+// reportWebVitals(console.log);
