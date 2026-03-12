1653f0cf0c72b7b9c8c14144840fb70e97f286ce
emianna2005 <emiliaannaantony@gmail.com>
2026-03-12 11:24:54 +0530

    Resolved merge conflict and keep Home only

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-import LoginSignup from "./LoginSignup";
-
-function App() {
-  return <LoginSignup />;
-=======
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
->>>>>>> 4c04940fbf6fc09b1049bccb8b98e6a589491d12
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
